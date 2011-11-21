412f5be3fe6ec191148f12222c46a228c64a3973
Jonas Strandstedt <jonas.strandstedt@gmail.com>
2011-11-21 09:50:32 +0100

    Added debug text and logging

--- application/views/startsida.php
+++ application/views/startsida.php
@@ -1,5 +1,18 @@
 Startsidan
 data = {data}
+<br><br>
+<?php echo site_url(); ?><br />
+<?php  echo uri_string(); ?><br />
+<?php echo current_url(); ?><br />
+<?php  echo base_url(); ?><br />
+
+URI String (1 to 5):<br />
+1: <?php echo $this->uri->segment(1); ?><br />
+2: <?php echo $this->uri->segment(2); ?><br />
+3: <?php echo $this->uri->segment(3); ?><br />
+4: <?php echo $this->uri->segment(4); ?><br />
+5: <?php echo $this->uri->segment(5); ?>
+<hr>
 <pre>
 <?php var_dump($data) ?>
 </pre>
