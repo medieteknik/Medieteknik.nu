1d1343fbe35c6a34c5a60a61fed3cf1be9bee69d
Klas Eskilson <klas.eskilson@gmail.com>
2012-09-22 15:44:27 +0200

    lade till twitter på användarprofilerna samt påbörjat viss uppdelning av css:erna för lättare hantering

--- application/views/user_profile.php
+++ application/views/user_profile.php
@@ -1,12 +1,22 @@
 <?php
 
 echo '
-<div class="main-box clearfix">
+<div class="main-box clearfix profile">
 	<h2>',get_full_name($user),'</h2>
-	<ul>
-		<li>Web: ',$user->web,'</li>
-		<li>linkedin: ',$user->linkedin,'</li>
-	</ul>
-	<p>',$user->presentation,'</p>
-</div>';
+		<div class="profile-content">
+		<div class="profile-link">
+			<span class="link-descr">Web</span>
+			<span class="link-link">
+				<a href="',$user->web,'" target="_blank">',$user->web,'</a>
+			</span>
+		</div><!-- close .profile-link -->
+		<div class="profile-link">
+			<span class="link-descr">Linkedin</span>
+			<span class="link-link">
+				<a href="',$user->linkedin,'" target="_blank">',$user->linkedin,'</a>
+			</span>
+		</div><!-- close .profile-link -->
+		<p>',$user->presentation,'</p>
+	</div>
+</div><!-- close .main-box -->';
 
