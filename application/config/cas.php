55a12b4ad40a65cc631585d4b5aad6fda0834a8b
Mikael Zackrisson <micke.zackrisson@gmail.com>
2013-10-06 13:11:50 +0200

    cas config added

---
+++ application/config/cas.php
@@ -0,0 +1,5 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$config['cas_server_url'] = 'https://login.liu.se/cas';
+$config['phpcas_path'] = './CAS/CAS-1.3.2';
+$config['cas_disable_server_validation'] = TRUE;
+// $config['cas_debug'] = TRUE; // <--  use this to enable phpCAS debug mode
