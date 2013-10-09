de2a42e6f0c374fd15a98f592e86e18d6a28e52e
Mikael Zackrisson <micke.zackrisson@gmail.com>
2013-10-09 23:14:37 +0200

    added admin privileges to me (Mikael)

--- application/models/install_model.php
+++ application/models/install_model.php
@@ -771,6 +771,8 @@ class Install_model extends CI_Model
 			$this->db->insert('users_privileges', $data);
 			$data = array('user_id' => 6,'privilege_id' => 1);
 			$this->db->insert('users_privileges', $data);
+			$data = array('user_id' => 9,'privilege_id' => 1);
+			$this->db->insert('users_privileges', $data);
 
 			// news_post
 			$data = array('user_id' => 3,'privilege_id' => 4);
