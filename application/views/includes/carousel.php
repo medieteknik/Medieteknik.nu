b7ff07df1bfaf2fc5d00ae84082d312d259bfb20
Klas Eskilson <klas.eskilson@gmail.com>
2014-03-01 18:13:47 +0100

    accidental removal of file fixed and added new carousel file

---
+++ application/views/includes/carousel.php
@@ -0,0 +1,49 @@
+<!-- <div class="main-box featured clearfix">
+    <div class="oneThird">
+        <h2>Civilingenjör i Medieteknik – en utbildning för dig?</h2>
+        <p>
+            Aenean tincidunt dui lacus. Ut tincidunt auctor tellus id fermentum. Donec eget turpis augue. Mauris at ipsum est. Donec pharetra adipiscing rhoncus. Ut non condimentum leo.
+        </p>
+    </div>
+    <img class="twoThirds" src="<?php echo base_url(); ?>web/img/carouselle.png" alt="carouselle"/>
+</div> -->
+<div id="big-carousel" class="carousel slide darkbg" data-ride="carousel">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+	</ol>
+
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner">
+		<div class="item active">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-4">
+						<h1>
+							Civilingenjör i Medieteknik – en utbildning för dig?
+						</h1>
+					</div>
+					<div class="col-sm-8">
+						<iframe src="//player.vimeo.com/video/73557097?title=0&amp;byline=0&amp;portrait=0&amp;color=a6a6a6" width="100%" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="item">
+			<div class="container">
+				<h1>
+					Civilingenjör i Medieteknik – en utbildning för dig?
+				</h1>
+			</div>
+		</div>
+	</div>
+
+	<!-- Controls -->
+	<!-- <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+	</a>
+	<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+	</a> -->
+</div>
