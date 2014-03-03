010ada77aacb7e61ce17092143cb895832cddd94
Klas Eskilson <klas.eskilson@gmail.com>
2014-03-04 00:03:32 +0100

    Final touch on user profile

--- application/views/user_profile.php
+++ application/views/user_profile.php
@@ -5,7 +5,7 @@ $editbutton = "";
 
 // check if user is viewing it's own profile
 if($is_logged_in) {
-	$editbutton = anchor('/user/edit_profile/', $lang['profile_edit']);
+	$editbutton = '<i>'.anchor('/user/edit_profile/', $lang['profile_edit']).'</i>';
 }
 
 $readweb = str_replace(array('http://', 'https://'), '', $user->web);
@@ -16,12 +16,13 @@ echo '
 		'<div class="col-sm-3">',
 			'<p>',gravatarimg($user, 300, ' class="img-responsive img-circle"'),'</p>',
 		'</div>',
-		'<div class="col-sm-6">',
+		'<div class="col-sm-9">',
 			'<h3>',get_full_name($user),' ',$editbutton,'</h3>',
 			'<ul class="profilelinks list-inline">',
-				($user->linkedin ? '<li><a href="'.$user->linkedin.'" target="_blank">LinkedIn</a></li>': ''),
-				($user->twitter ? '<li><a href="https://twitter.com/'.$user->twitter.'" target="_blank">Twitter @'.$user->twitter.'</a></li>': ''),
+				($user->linkedin ? '<li><a href="'.$user->linkedin.'" target="_blank">Linkedin</a></li>': ''),
+				($user->twitter ? '<li><a href="https://twitter.com/'.$user->twitter.'" target="_blank">Twitter <i>@'.$user->twitter.'</i></a></li>': ''),
 				($user->web ? '<li><a href="'.$user->web.'" target="_blank">'.$readweb.'</a></li>': ''),
+				'<li><a href="mailto:',$user->lukasid,'@student.liu.se" target="_blank"><i>',$user->lukasid,'@student.liu.se</i></a></li>',
 			'</ul>',
 			'<p>',text_format($user->presentation),'</p>',
 		'</div>',
