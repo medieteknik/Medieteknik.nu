b923987bdf1cbb99949379c1e9bea4dd7806b97b
Klas Eskilson <klas.eskilson@gmail.com>
2014-03-03 12:00:19 +0100

    updated carousel a bit

--- application/views/includes/carousel.php
+++ application/views/includes/carousel.php
@@ -1,49 +1,70 @@
-<!-- <div class="main-box featured clearfix">
-    <div class="oneThird">
-        <h2>Civilingenjör i Medieteknik – en utbildning för dig?</h2>
-        <p>
-            Aenean tincidunt dui lacus. Ut tincidunt auctor tellus id fermentum. Donec eget turpis augue. Mauris at ipsum est. Donec pharetra adipiscing rhoncus. Ut non condimentum leo.
-        </p>
-    </div>
-    <img class="twoThirds" src="<?php echo base_url(); ?>web/img/carouselle.png" alt="carouselle"/>
-</div> -->
-<div id="big-carousel" class="carousel slide darkbg" data-ride="carousel">
+<div id="big-carousel" class="carousel slide" data-ride="carousel">
 	<!-- Indicators -->
 	<ol class="carousel-indicators">
-		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		<li data-target="#big-carousel" data-slide-to="0" class="active"></li>
+		<li data-target="#big-carousel" data-slide-to="1"></li>
 	</ol>
 
 	<!-- Wrapper for slides -->
 	<div class="carousel-inner">
-		<div class="item active">
+		<div class="item active darkbg">
 			<div class="container">
 				<div class="row">
-					<div class="col-sm-4">
-						<h1>
+					<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+						<h1 class="text-center">
 							Civilingenjör i Medieteknik – en utbildning för dig?
 						</h1>
 					</div>
-					<div class="col-sm-8">
-						<iframe src="//player.vimeo.com/video/73557097?title=0&amp;byline=0&amp;portrait=0&amp;color=a6a6a6" width="100%" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+				</div>
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1">
+						<iframe src="//player.vimeo.com/video/73557097?title=0&amp;byline=0&amp;portrait=0&amp;color=a6a6a6"
+							width="100%" height="360" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
 					</div>
 				</div>
 			</div>
 		</div>
-		<div class="item">
-			<div class="container">
-				<h1>
-					Civilingenjör i Medieteknik – en utbildning för dig?
-				</h1>
+		<?php
+		$photos = array(
+					array(
+						'file' => 'campus-1-peter-modin-liu.jpg',
+						'photo' => 'Peter Modin, LiU',
+						'link' => 'http://liu.se'
+						 ),
+					array(
+						'file' => 'campus-2-peter-modin-liu.jpg',
+						'photo' => 'Peter Modin, LiU',
+						'link' => 'http://liu.se'
+						 ),
+					array(
+						'file' => 'campus-3-peter-modin-liu.jpg',
+						'photo' => 'Peter Modin, LiU',
+						'link' => 'http://liu.se'
+						 ),
+					array(
+						'file' => 'campus-bibl.jpg',
+						'photo' => 'Norrköpings stadsbibliotek',
+						'link' => 'http://www.flickr.com/photos/gamle_swartzen/486774017/'
+						 )
+				  );
+		$img = rand(0, count($photos)-1);
+		?>
+		<div class="item photo" style="background-image: url(<?php echo base_url().'web/img/campus/'.$photos[$img]['file']; ?>);">
+			<div class="carousel-caption">
+				<h1>Linköpings universitet &ndash; Campus Norrköping</h1>
+				<p class="lead">
+					Civilingenjör i Medieteknik på Linköpings universitet ges på Campus Norrköping &ndash; Sveriges bästa studentstad 2013.<br />
+					<a href="<?php echo $photos[$img]['link']; ?>" target="_blank">Foto: <?php echo $photos[$img]['photo']; ?></a>
+				</p>
 			</div>
 		</div>
 	</div>
 
 	<!-- Controls -->
-	<!-- <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+	<a class="left carousel-control hidden-sm hidden-xs" href="#big-carousel" data-slide="prev">
 		<span class="glyphicon glyphicon-chevron-left"></span>
 	</a>
-	<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+	<a class="right carousel-control hidden-sm hidden-xs" href="#big-carousel" data-slide="next">
 		<span class="glyphicon glyphicon-chevron-right"></span>
-	</a> -->
+	</a>
 </div>
