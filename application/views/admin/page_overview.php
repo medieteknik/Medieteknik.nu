4fae6fd937673245bdbc8cca72155d851eef15ed
Klas Eskilson <klas.eskilson@gmail.com>
2014-03-09 21:37:07 +0100

    Minor update to page overview

--- application/views/admin/page_overview.php
+++ application/views/admin/page_overview.php
@@ -7,18 +7,22 @@ echo '
 	<h3>',$lang['admin_addpage'],'</h3>
 	<p>',anchor('admin/page/create', $lang['admin_createnewpagebyclicking']),'</p>
 </div>';
-
-foreach($page_array as $page) {
-	?>
-	<div class="main-box clearfix margin-top">
-		<h3>
-			<?php
-			echo anchor('admin/page/edit/'.$page->id, $page->name, array('title' => $lang['page_editthepage']));
-			echo !$page->published ? ' <span class="label label-default">'.$lang['misc_draft'].'</span>' : '';
-			?>
-		</h3>
-	</div>
-	<?php
-}
-
-// do_dump($page_array);
+?>
+<div class="main-box clearfix margin-top">
+	<h3>
+		<?php
+		echo $lang['admin_adminpage'];
+		?>
+	</h3>
+	<ul class="list-unstyled box-list">
+		<?php
+		foreach($page_array as $page)
+		{
+			$draft = !$page->published ? ' <span class="label label-default">'.$lang['misc_draft'].'</span>' : '';
+			echo '<li>';
+			echo anchor('admin/page/edit/'.$page->id, $page->name.$draft, array('title' => $lang['page_editthepage']));
+			echo '</li>';
+		}
+		?>
+	</ul>
+</div>
