e2f03910222fcca81899bbdb4b04a04204588a0d
Klas Eskilson <klas.eskilson@gmail.com>
2014-04-06 22:24:02 +0200

    styled email a bit

--- application/views/templates/email.php
+++ application/views/templates/email.php
@@ -1,12 +1,28 @@
 <html>
 	<head>
 		<style>
+			body {
+				width: 100%;
+				background-color: #f4f4f4;
+				color: #666;
+			}
+			body p {
+				margin: 5px 15px;
+			}
+			body footer {
+				background-color: #E8E8E9;
+			}
+			body footer p {
+				line-height: 30px;
+			}
 		</style>
 	</head>
 	<body>
 		<?php echo $message; ?>
 		<footer>
-			<?php echo $lang['email_footer']; ?>
+			<p>
+				<?php echo base_url('web/img/email_logo.png'); ?> <a href="<?php echo base_url(); ?>">medieteknik.nu</a>
+			</p>
 		</footer>
 	</body>
 </html>
