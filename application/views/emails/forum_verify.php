4a0b606947d1a2d21d000b2b96c776206b8f1c9f
Klas Eskilson <klas.eskilson@gmail.com>
2014-04-06 23:21:37 +0200

    Final draft. Removed most styling.

--- application/views/emails/forum_verify.php
+++ application/views/emails/forum_verify.php
@@ -1,27 +1,27 @@
 <header>
-	<h2 style="color: #666; margin: 15px;">
+	<h2>
 		<?php echo $lang['email_forum_verify_title']; ?>
 	</h2>
 </header>
 <main>
-	<p style="margin: 15px; color: #666;">
+	<p>
 		<?php echo $lang['email_hello'].', '.$data['name']; ?>!
 	</p>
-	<p style="margin: 15px; color: #666;">
+	<p>
 		<?php echo $lang['email_forum_verify_explanation']; ?>
 	</p>
-	<p style="margin: 15px; color: #666;">
+	<p>
 		<?php
 		$link = 'forum/verify/'.urlencode($data['email']).'/'.$data['hash'];
-		echo anchor($link, $lang['email_forum_verify_confirm'], array('style' => 'margin:0; padding: 10px 16px; font-size: 18px; border-radius: 6px; text-decoration: none; background-color: #f4f4f4; border: 1px solid #aaaaaa;'));
+		echo anchor($link, $lang['email_forum_verify_confirm']);
 		?>
 	</p>
-	<p style="margin: 15px; color: #666;">
+	<p>
 		<small>
 			<?php echo $lang['email_forum_verify_ifitdoesntwork'].'{unwrap}'.base_url($link).'{/unwrap}'; ?>
 		</small>
 	</p>
-	<p style="margin: 15px; color: #666;">
+	<p>
 		<?php echo $lang['email_byebye']; ?>,<br>
 		<?php echo $lang['email_assoc']; ?>
 	</p>
--- application/views/templates/email.php
+++ application/views/templates/email.php
@@ -1,10 +1,12 @@
 <html>
-	<body style="width: 100%;">
+	<body>
 		<?php echo $message; ?>
-		<footer style="background-color: #E8E8E9;">
-			<p style="line-height: 30px; color: #666; margin: 15px;">
-				<img src="<?php echo base_url('web/img/email_logo.png'); ?>" style="float: left; margin-right: 10px;">
-				<a href="<?php echo base_url(); ?>" style="color: #666; text-transform: uppercase;">medieteknik.nu</a>
+		<footer>
+			<p>
+				<a href="<?php echo base_url(); ?>" style="color: #666; text-transform: uppercase; text-decoration: none;">
+					<img src="<?php echo base_url('web/img/email_logo.png'); ?>" style="float: left; margin-right: 10px;">
+					medieteknik.nu
+				</a>
 			</p>
 		</footer>
 	</body>
