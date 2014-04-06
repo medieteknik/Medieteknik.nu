40f74ae8781529e51f1e32781760ffcebbe78fa4
Klas Eskilson <klas.eskilson@gmail.com>
2014-04-06 22:40:52 +0200

    Fixed email styling with some new knowledge

--- application/views/emails/forum_verify.php
+++ application/views/emails/forum_verify.php
@@ -2,24 +2,24 @@
 	<h2><?php echo $lang['email_forum_verify_title']; ?></h2>
 </header>
 <main>
-	<p>
+	<p style="margin: 5px 15px; color: #666;">
 		<?php echo $lang['email_hello'].', '.$data['name']; ?>!
 	</p>
 	<p>
 		<?php echo $lang['email_forum_verify_explanation']; ?>
 	</p>
-	<p>
+	<p style="margin: 5px 15px; color: #666;">
 		<?php
 		$link = 'forum/verify/'.urlencode($data['email']).'/'.$data['hash'];
 		echo anchor($link, $lang['email_forum_verify_confirm'], array('class' => 'button'));
 		?>
 	</p>
-	<p>
+	<p style="margin: 5px 15px; color: #666;">
 		<small>
 			<?php echo $lang['email_forum_verify_ifitdoesntwork'].'{unwrap}'.base_url($link).'{/unwrap}'; ?>
 		</small>
 	</p>
-	<p>
+	<p style="margin: 5px 15px; color: #666;">
 		<?php echo $lang['email_byebye']; ?>,<br>
 		<?php echo $lang['email_assoc']; ?>
 	</p>
--- application/views/templates/email.php
+++ application/views/templates/email.php
@@ -1,27 +1,12 @@
 <html>
 	<head>
-		<style>
-			body {
-				width: 100%;
-				background-color: #f4f4f4;
-				color: #666;
-			}
-			body p {
-				margin: 5px 15px;
-			}
-			body footer {
-				background-color: #E8E8E9;
-			}
-			body footer p {
-				line-height: 30px;
-			}
-		</style>
 	</head>
-	<body>
+	<body style="width: 100%; background-color: #f4f4f4;">
 		<?php echo $message; ?>
-		<footer>
-			<p>
-				<?php echo base_url('web/img/email_logo.png'); ?> <a href="<?php echo base_url(); ?>">medieteknik.nu</a>
+		<footer style="background-color: #E8E8E9;">
+			<p style="line-height: 30px; color: #666; margin: 15px;">
+				<img src="<?php echo base_url('web/img/email_logo.png'); ?>" style="float: left;">
+				<a href="<?php echo base_url(); ?>">medieteknik.nu</a>
 			</p>
 		</footer>
 	</body>
