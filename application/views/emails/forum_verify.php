db598adc0eb52cdb97da6ccb564ac49e65a88351
Klas Eskilson <klas.eskilson@gmail.com>
2014-04-06 22:59:45 +0200

    Further styling after some detected bugs

--- application/views/emails/forum_verify.php
+++ application/views/emails/forum_verify.php
@@ -1,25 +1,27 @@
 <header>
-	<h2><?php echo $lang['email_forum_verify_title']; ?></h2>
+	<h2 style="color: #666; margin: 15px;">
+		<?php echo $lang['email_forum_verify_title']; ?>
+	</h2>
 </header>
 <main>
-	<p style="margin: 5px 15px; color: #666;">
+	<p style="margin: 15px; color: #666;">
 		<?php echo $lang['email_hello'].', '.$data['name']; ?>!
 	</p>
-	<p>
+	<p style="margin: 15px; color: #666;">
 		<?php echo $lang['email_forum_verify_explanation']; ?>
 	</p>
-	<p style="margin: 5px 15px; color: #666;">
+	<p style="margin: 15px; color: #666;">
 		<?php
 		$link = 'forum/verify/'.urlencode($data['email']).'/'.$data['hash'];
-		echo anchor($link, $lang['email_forum_verify_confirm'], array('class' => 'button'));
+		echo anchor($link, $lang['email_forum_verify_confirm'], array('style' => 'margin:0; padding: 10px 16px; font-size: 18px; border-radius: 6px; text-decoration: none; background-color: #f4f4f4; border: 1px solid #aaaaaa;'));
 		?>
 	</p>
-	<p style="margin: 5px 15px; color: #666;">
+	<p style="margin: 15px; color: #666;">
 		<small>
 			<?php echo $lang['email_forum_verify_ifitdoesntwork'].'{unwrap}'.base_url($link).'{/unwrap}'; ?>
 		</small>
 	</p>
-	<p style="margin: 5px 15px; color: #666;">
+	<p style="margin: 15px; color: #666;">
 		<?php echo $lang['email_byebye']; ?>,<br>
 		<?php echo $lang['email_assoc']; ?>
 	</p>
--- application/views/templates/email.php
+++ application/views/templates/email.php
@@ -1,12 +1,10 @@
 <html>
-	<head>
-	</head>
-	<body style="width: 100%; background-color: #f4f4f4;">
+	<body style="width: 100%;">
 		<?php echo $message; ?>
 		<footer style="background-color: #E8E8E9;">
 			<p style="line-height: 30px; color: #666; margin: 15px;">
-				<img src="<?php echo base_url('web/img/email_logo.png'); ?>" style="float: left;">
-				<a href="<?php echo base_url(); ?>">medieteknik.nu</a>
+				<img src="<?php echo base_url('web/img/email_logo.png'); ?>" style="float: left; margin-right: 10px;">
+				<a href="<?php echo base_url(); ?>" style="color: #666; text-transform: uppercase;">medieteknik.nu</a>
 			</p>
 		</footer>
 	</body>
