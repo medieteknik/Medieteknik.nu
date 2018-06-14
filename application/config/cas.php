53c6f655fd9c6aa6d211f007044e174d0aa88fe3
SaraOlsson <sarol688@student.liu.se>
2018-06-14 15:07:21 +0200

    Update cas.php

--- application/config/cas.php
+++ application/config/cas.php
@@ -1,5 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$config['cas_server_url'] = 'https://login.liu.se/cas';
+$config['cas_server_url'] = 'https://login.it.liu.se/idp/profile/cas';
 $config['phpcas_path'] = './CAS/CAS-1.3.2';
 $config['cas_disable_server_validation'] = TRUE;
 // $config['cas_debug'] = TRUE; // <--  use this to enable phpCAS debug mode
